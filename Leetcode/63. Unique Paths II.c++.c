9cf9af524f52d9bbca7e3e633b19af34e44086f8
Dinesh543 <56669337+Dinesh543@users.noreply.github.com>
2020-08-13 13:10:55 +0530

    Create 63. Unique Paths II.c++.c

---
+++ Leetcode/63. Unique Paths II.c++.c
@@ -0,0 +1,57 @@
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+Output: 2
+Explanation:
+There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+
+solution ::
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
+        int n = Grid.size();
+        int m = Grid[0].size();
+        vector <vector<int>> dp(n,vector<int>(m,0));
+        for (int i = 0; i < m; i++) {
+            if (Grid[0][i] == 0) {
+                dp[0][i] = 1;
+            }else {
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (Grid[i][0] == 0) {
+                dp[i][0] = 1;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (Grid[i][j] == 0) {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
